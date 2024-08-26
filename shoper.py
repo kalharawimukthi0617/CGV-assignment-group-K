41c89d5f17fc058eb5d3c8e06cff0c7ab8ebd563
kadsilva <kadsilva@students.nsbm.ac.lk>
2024-08-26 19:47:35 +0530

    create function to image convert to the grey image

--- shoper.py
+++ shoper.py
@@ -10,6 +10,10 @@ class ShopBill:
         self.imagePath = imagePath
         self.image = cv2.imread(imagePath)
 
+        self.convertTograyImage(self.image)
+
+    def convertTograyImage(self, img):
+        self.gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 if __name__ == "__main__":
     ShopBill()
