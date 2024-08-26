96d56d00fc0b6224ba87523714859fce7a6c76d6
kadsilva <kadsilva@students.nsbm.ac.lk>
2024-08-26 19:57:00 +0530

    show the grey image

--- shoper.py
+++ shoper.py
@@ -12,6 +12,11 @@ class ShopBill:
 
         self.convertTograyImage(self.image)
 
+        #show the grey image
+        cv2.imshow('Processed Image', self.gray_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
     def convertTograyImage(self, img):
         self.gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
