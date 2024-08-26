8069cddb56c65ef6c26ef75a400f6924066bcab6
Darshana75 <84192420+Darshana75@users.noreply.github.com>
2024-08-26 22:20:22 +0530

    Create function to adjust image contrast

--- shoper.py
+++ shoper.py
@@ -11,6 +11,7 @@ class ShopBill:
         self.image = cv2.imread(imagePath)
 
         self.convertTograyImage(self.image)
+        self.increaseContrast(self.gray_image) 
 
         #show the grey image
         cv2.imshow('Processed Image', self.gray_image)
@@ -19,6 +20,9 @@ class ShopBill:
 
     def convertTograyImage(self, img):
         self.gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    def increaseContrast(self, img):
+        self.contrast_image = cv2.addWeighted(img, 1.5, np.zeros(img.shape, img.dtype), 0, -50)
 
 if __name__ == "__main__":
     ShopBill()
